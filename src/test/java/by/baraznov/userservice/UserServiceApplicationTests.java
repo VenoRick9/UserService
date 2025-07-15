382c193aede248bc5d0d2c58d970db88a6b80cb0
VenoRick9 <danila.baraznou@innowise.com>
2025-07-15 09:01:58 +0300

    rebuild test

--- src/test/java/by/baraznov/userservice/UserServiceApplicationTests.java
+++ src/test/java/by/baraznov/userservice/UserServiceApplicationTests.java
@@ -1,11 +1,15 @@
 package by.baraznov.userservice;
 
+import by.baraznov.userservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Import(TestContainersConfig.class)
 @Testcontainers
+
 class UserServiceApplicationTests {
 
     @Test
