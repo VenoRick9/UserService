3d77f5affb9efe101bb794459a982f79d78768f2
VenoRick9 <danila.baraznou@innowise.com>
2025-07-31 11:06:34 +0300

    fix n+1

--- src/main/java/by/baraznov/userservice/repositories/UserRepository.java
+++ src/main/java/by/baraznov/userservice/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package by.baraznov.userservice.repositories;
 
 import by.baraznov.userservice.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +13,18 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.cards WHERE u.email = :email")
     Optional<User> findUserByEmail(String email);
 
-    @Query(value = "SELECT * FROM users AS u WHERE u.id IN :ids", nativeQuery = true)
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.cards WHERE u.id IN :ids")
     List<User> findUsersByIds(@Param(value = "ids") List<Integer> ids);
+
+    @Override
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.cards WHERE u.id = :id")
+    Optional<User> findById(Integer id);
+
+    @Override
+    @Query(value = "SELECT u FROM User u LEFT JOIN FETCH u.cards",
+            countQuery = "SELECT COUNT(u) FROM User u")
+    Page<User> findAll(Pageable pageable);
 }
